8d43caaa5fdcbf6ef4da4eb05605b141f1521d79
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-13 17:46:58 -0300

    object

---
+++ modulos/guia-estelar-javascript/tipos-de-dados/object.js
@@ -0,0 +1,19 @@
+/*
+    Object * objeto
+
+    Propriedades / Atributos
+
+    Funcionalidades / Métodos
+
+    { prioridade: "valor" }
+*/
+ 
+console.log({
+    name: "Thalyson",
+    idade: 16,
+    programar: function() {
+        console.log('Programar')
+    }
+})
+
+
