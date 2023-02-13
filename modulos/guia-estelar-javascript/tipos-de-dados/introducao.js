f134d7f8d4b8af571046ef42fcd77d59b6f0f8c9
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-13 17:46:52 -0300

    introducao

---
+++ modulos/guia-estelar-javascript/tipos-de-dados/introducao.js
@@ -0,0 +1,24 @@
+/*
+    Tipos de dados
+
+    Grámatica
+        Elementos da linguagem e suas combinações
+
+        A arte de falar e escrever corretamente
+
+    Vocabulário
+        Conjunto de termos e expressões
+
+        Agrupamento de palavras
+
+    Precisamos saber como escrever
+    Precisamos saber os significados
+    Precisamos continuar aprendendo, para crescer nosso vocabulário
+
+    Como vai ser a dinâmica do aprendizado?
+
+    Conceitos e escrita
+    
+    Vamos aprender os tipos de dados utilizados na linguagem
+        Você sábia que é possível aprender 80% de uma lingua nova, com cerca de 20% do vocabulário? (até menos)
+*/
