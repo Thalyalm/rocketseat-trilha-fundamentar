df0e24ce44550b1a48220d68f4a5789887ad1ff6
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-13 17:46:46 -0300

    number

---
+++ modulos/guia-estelar-javascript/tipos-de-dados/number.js
@@ -0,0 +1,14 @@
+/*
+    Number *numeros
+
+    33 // inteiros
+    12.5 // reais
+    NaN // Not a Number
+    Infinity // infinito
+*/
+
+console.log(33 + 12.5)
+
+console.log(33 / "abc")
+
+console.log(Infinity)
