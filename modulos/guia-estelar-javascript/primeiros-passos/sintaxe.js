c375129122ab8049ac6a2c6229f145a4f8a284f1
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-13 16:58:31 -0300

    sintaxe

---
+++ modulos/guia-estelar-javascript/primeiros-passos/sintaxe.js
@@ -0,0 +1,15 @@
+/*
+    A importância da sintaxe
+
+    Toda linguagem de sintaxe
+
+    Uma boa comunicação, necessita de um boa sintaxe
+
+    82% dos erros para iniciantes em programação
+*/
+
+console.log("Olá, mundo!")
+
+//consolelog("Olá, mundo!")
+
+//consolelog.("Olá, mundo!)"
