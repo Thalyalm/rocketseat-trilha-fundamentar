bb6eb0202ea0e24746ae18745ca9bf89bcae29cf
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-13 14:20:13 -0300

    javascript

---
+++ modulos/guia-estelar-javascript/introducao/javascript.js
@@ -0,0 +1,44 @@
+/*
+    JavaScript
+
+    Linguagem de programação que roda no navegador do usuário (front-end)
+        Se você clicar em algum botão da página e aparece uma janela. Isso é o JavaScript
+
+        Alteração do site ou aplicativo, conforme a interação do usuário
+
+    Roda também no computador (back-end).
+
+    O que podemos fazer?
+        Podemos criar aplicações web, mobile (React Native), desktop (Electron)
+
+    Empresas famosas usam:
+        Facebook (Instagram, WhatsApp)
+        Google (Youtube, Gmail, Drive)
+        Uber
+        Netflix
+        Tiktok
+        ...
+
+    99,99% dos sites na web usam JavaScript
+
+    Linguagem obrigatória para quem programa Front-end web
+
+    Evolução
+        A comunidade cresce cada vez mais e a linguagem está sempre evoluindo
+
+    O que vamos ver no curso?
+        Tipos de dados
+        Variáveis
+        Funções
+        Condicionais
+        Estruturas de repetição
+        ...
+
+    Como vai funcionar a dinâmica desse curso?
+        Treino de futebol
+        Treino de artes marciais
+
+        Conceitos e fundamentos
+        Prática
+        Revisão
+*/
