7a5b67a24d4008f23e8c43d79f5191c60e0b2aaa
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-13 17:47:13 -0300

    undefined vs null

---
+++ modulos/guia-estelar-javascript/tipos-de-dados/undefined-vs-null.js
@@ -0,0 +1,14 @@
+/*
+    Undefined * indefinido
+
+    null * nulo
+        Objeto que não possui nada dentro
+
+        Diferente de indefinido
+*/
+
+console.log(undefined)
+
+console.log(null)
+
+
