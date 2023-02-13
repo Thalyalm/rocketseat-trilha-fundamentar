edba8822b7cdd51278ba37c0f68eca6319281f39
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-13 14:20:06 -0300

    abertura

---
+++ modulos/guia-estelar-javascript/introducao/abertura.js
@@ -0,0 +1,5 @@
+/*
+    Nesse curso vamos aprender sobre JavaScript, entender os tipos de dados, variáveis, funções, manipulação de dados, pra que precisamos de todos esses recursos, e mais.
+
+    JavaScript é uma linguagem de programação que roda no navegador do usuário, e pode rodar no computador com algumas aplicações que serão citadas em seguida no curso. Com o JavaScript podemos criar aplicações web, mobile com React Native, e desktop com Electron. Neste curso vamos entender conceitos e fundamentos, praticaremos eles e depois revisaremos.
+*/
