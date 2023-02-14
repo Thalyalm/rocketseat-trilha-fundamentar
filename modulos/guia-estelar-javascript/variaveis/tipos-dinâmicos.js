efbbc9e805a6633be388341ccb18e81945e5e711
Thalyson Almeida <th.diogo04@gmail.com>
2023-02-14 11:09:47 -0300

    tipos dinamicos

---
+++ modulos/guia-estelar-javascript/variaveis/tipos-dinâmicos.js
@@ -0,0 +1,15 @@
+/*
+    Tipos dinâmicos
+
+    O JavaScript é uma linguagem fracamente tipada e dinâmica, ou seja, ao declarar uma variável não é necessário estabelecer um tipo específico e o tipo do valor da variável pode mudar conforme o código.
+*/
+
+let nome = "" //string
+
+console.log(typeof nome)
+
+let idade = 19
+
+console.log(typeof idade) //number
+
+idade = "19" //string
